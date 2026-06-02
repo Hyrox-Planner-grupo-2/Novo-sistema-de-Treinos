2c3eeee453f7ea69ea032c0c0c796418e4fdc239
João Carlos Sampaio <82887910+joaocssampaio@users.noreply.github.com>
2026-06-02 08:28:25 -0300

    parte_de_joca.py
    melhorei comunicação entre crude e controle de
    desempenhos

--- codigo atual.py
+++ codigo atual.py
@@ -60,7 +60,7 @@ def tipoDEtreino():
             elif tipo_treino == 3:
                 tipo = "SIMULADO HYROX"
             
-        except ValueError, TypeError: 
+        except (ValueError, TypeError): 
             clear()
             print("Resposta inválida!")
             continue
@@ -109,7 +109,131 @@ def validar_data():
             print("Data inválida! Use o formato dd/mm/aaaa.\n")
             continue
 
+def controledesempenho(nomedotreino):
+
+    desemp = {} 
+
+    # ================= ABRE O ARQUIVO ===================
+ 
+    file = open("Sistema de Treinos.txt", "r")
+    conteudo = file.read()
+    file.close()
+
+    treinos = conteudo.split("\n\n")
+
+    if "EXERCICIOS:" in treinos[-1]: 
+
+        desempenhos = (treinos[-1]).split("\n---\n")
+
+        achou = False
+        for i in range(1, len(desempenhos)):
+            if nomedotreino + "\n" in desempenhos[i]:
+                esse_desempenho = desempenhos[i]
+                index = i
+                achou = True
+                break
+        
+        if achou:
+
+            linhas = esse_desempenho.split("\n")
+            
+            i = 1
+            while i <= len(linhas)-4:
+                desemp[linhas[i]] = [linhas[i+1].split("\t")[2:-1],
+                                    linhas[i+2].split("\t")[1:-1],
+                                    linhas[i+3].split("\t")[2:-1],
+                                    linhas[i+4].split("\t")[1:-1]]
+                i += 5
+        
+        else:
+            desempenhos.append("valor que vai ser trocado dps")
+            index = len(desempenhos)-1
+                
+    else:
+        desempenhos = ["EXERCICIOS:", "valor que vai ser trocado dps"]
+        index = 1
+        treinos.append(desempenhos)
+
+
+    # ====================== ADICIONAR NOVA LEITURA / CRIAR EXERCICIO =============
+
+    def add(exercicio):
+        #exercicio é o nome do exercicio
+
+        if exercicio not in desemp.keys():
+            desemp[exercicio] = [[],[],[],[]] #se o exercicio não existir ainda, criar uma matriz pra ele
+
+        tempo = input("Insira o tempo: ")
+        dist = input("Insira a distância: ")
+        carga = input("Insira a carga: ")
+        rep = input("Insira a quantidade de repetições: ")
+
+        desemp[exercicio][0].append(tempo)
+        desemp[exercicio][1].append(dist)
+        desemp[exercicio][2].append(carga)
+        desemp[exercicio][3].append(rep)
+
+        print(f"Exercício {exercicio} atualizado!\n")
+
+    # ====================== SALVAR MATRIZ =========================
+
+    def salvarmatriz():
+        dados = nomedotreino
+
+        for exercicio in desemp.keys():
+            dados += "\n" + exercicio.upper()
+
+            dados += ("\nTempos:\t\t")
+            for d in desemp[exercicio][0]:
+                dados += (d + "\t")
+
+            dados += ("\nDistâncias:\t")
+            for d in desemp[exercicio][1]:
+                dados += (d + "\t")
+
+            dados += ("\nCargas:\t\t")
+            for d in desemp[exercicio][2]:
+                dados += (d + "\t")
+
+            dados += ("\nRepetições:\t")
+            for d in desemp[exercicio][3]:
+                dados += (d + "\t")
+        
+        desempenhos[index] = dados
+
+        treinos.pop()
+        treinos.append("\n---\n".join(desempenhos))
+
+        conteudo = "\n\n".join(treinos)
+
+
+        file = open("Sistema de Treinos.txt", "w")
+        file.write(conteudo)
+        file.close()
+        
+    # ====================== LOOP PRINCIPAL =========================
+
+    print("EXERCICIOS e Controle de Desempenho\n")
+
+    cmd = "S"
+    while True:
+        if cmd == "S":
+            clear()
+            nome = input("Digite o nome do exercício que deseja adicionar/atualizar: ").upper()
+
+            add(nome)
+        elif cmd == "N":
+            salvarmatriz()
+            print("Encerrando programa...")
+            clear()
+            break
+        else:
+            print("comando não reconhecido.")
+        
+        cmd = input("Deseja continuar? (S/N) ").upper()
+
 def calcular_dias_restantes(data_):
+    import datetime
     try:
         data_evento = datetime.datetime.strptime(data_.strip(), "%d/%m/%Y").date()
         data_hoje = datetime.date.today()
@@ -138,20 +262,19 @@ def adicionar_competicao(comp,data,local,cat):
     arquivo.close()
 
 
-arquivo = open("Sistema de Treinos.txt", "a")
-arquivo.close()
 
 while True:
    print("==========BEM VINDO AO HYROX PLANNER==========\n")
    opcao_escolhida = input("Você deseja:" 
     "\n[1] Adicionar\n"
-      "[2] Visualizar treinos\n"
-      "[3] Visualizar competições \n"
-      "[4] Editar\n"
-      "[5] Excluir\n"
-      "[6] Adicionar competição \n"
-      "[7] Parar"
-      "\nRESPOSTA: ")
+        "[2] Visualizar treinos\n"
+        "[3] Visualizar competições \n"
+        "[4] Editar\n"
+        "[5] Excluir\n"
+        "[6] Controle de Desempenho\n"
+        "[7] Adicionar competição \n"
+        "[8] Parar"
+        "\nRESPOSTA: ")
 
    clear()
 
@@ -172,14 +295,28 @@ while True:
             
         clear()
         
-        treino = open("Sistema de Treinos.txt", "a")
-        treino.write("Dados do Treino:" 
+        treino = open("Sistema de Treinos.txt", "r")
+        conteudo = treino.read()
+        treino.close()
+
+        treino = open("Sistema de Treinos.txt", "w")
+
+        dados_novos = ("Dados do Treino:" 
                         "\nNOME DO TREINO: " + nome_treino.upper().strip() + 
                         "\nTIPO DE TREINO: " + tipo + 
                         "\nDATA DO TREINO: " + data_treino + 
                         "\nDURAÇÃO DO TREINO: " + duracao_treino +
-                        "\nINTENSIDADE DO TREINO: " + intensidade_final 
-                        + "\n\n")
+                        "\nINTENSIDADE DO TREINO: " + intensidade_final)
+        
+        treinos = conteudo.split("\n\n") # quebra o texto cru do arquivo em uma lista
+        if "EXERCICIOS:\n" in treinos[-1]:
+            treinos.insert(-1, dados_novos) # coloca os dados novos dentro da lista antes da parte de controle de desempenho
+        else:
+            treinos.append(dados_novos)
+        treino.write("\n\n".join(treinos)) # da .join na lista e sobreescreve o arquivo inteiro
+
+        # isso faz com que o controle de desempenho sempre fique na parte de baixo do arquivo (tem q ser assim pra o meu codigo funcionar)
+
         clear()
         print("Treino Adicionado com Sucesso! \n\n")
         treino = open("Sistema de Treinos.txt", "r")
@@ -194,35 +331,28 @@ while True:
         if not pergunta():
             break
 
-    elif opcao_escolhida == "3":
-            clear()
-            print("==========COMPETIÇÕES==========")
-    
-            try:
-                arquivo_comp = open("Competições.txt","r")
-                conteudo_comp = arquivo_comp.read()
-                arquivo_comp.close()
-    
-                if conteudo_comp.strip() =="":
-                    print("Nenhuma competição cadastrada")
-                else:
-                    print(conteudo_comp)
-    
-            except FileNotFoundError:
-                print("Nenhuma competição cadastrada ainda.")
-                      
-            while True:
-                resposta = input("\nVocê quer continuar? s/n \nRESPOSTA: ").lower().strip()
-                
-                if resposta == "s":
-                    clear()
-                    break
-                elif resposta == "n":
-                    print("Programa Finalizado!")
-                    exit()
-                else:
-                    print("RESPOSTA INVÁLIDA! Digite apenas 's' para sim ou 'n' para não.")
-                    
+
+   elif opcao_escolhida == "3":
+        clear()
+        print("==========COMPETIÇÕES==========")
+
+        try:
+            arquivo_comp = open("Competições.txt","r")
+            conteudo_comp = arquivo_comp.read()
+            arquivo_comp.close()
+
+            if conteudo_comp.strip() =="":
+                print("Nenhuma competição cadastrada")
+            else:
+                print(conteudo_comp)
+
+        except FileNotFoundError:
+            print("Nenhuma competição cadastrada ainda.")
+                  
+        if not pergunta():
+            break
+
+
    elif opcao_escolhida == "4":
         conteudo = abrir_leitura()
         treinos = conteudo.split("\n\n")
@@ -231,7 +361,7 @@ while True:
             try:
                 treino_antigo = input("Digite qual treino deseja editar: ")
             except:    
-                if f"NOME DO TREINO: {treino_antigo.upper()}" not in conteudo:
+                if f"NOME DO TREINO: {treino_antigo.upper()}\n" not in conteudo:
                     print("Treino Inexistente!!\n\n")
                     continue
             else:
@@ -239,6 +369,33 @@ while True:
                 break
             
         treino_novo = nomeDOtreino(conteudo)
+
+        # ========================================
+        # Essa parte do código procura o treino antigo no controle de desempenhos
+
+        if "EXERCICIOS:" in treinos[-1]: 
+
+            desempenhos = (treinos[-1]).split("\n---\n")
+
+            achou = False
+            for o in range(1, len(desempenhos)):
+                if treino_antigo.lower() + "\n" in desempenhos[o]:
+                    esse_desempenho = desempenhos[o]
+                    index = o
+                    achou = True
+                    break
+            
+            if achou:
+                linhas = esse_desempenho.split("\n")
+                linhas[0] = treino_novo.lower().strip() # atualiza a primeira linha de esse_desempenho
+
+                desempenhos[o] = "\n".join(linhas) # junta as linhas e coloca em desempenhoS
+
+                treinos[-1] = "\n---\n".join(desempenhos) # atualiza o último índice de treinos
+
+                # e veja só que maravilha! eu não presciso colocar < "\n\n".join(treinos) > no arquivo de volta, pq o código de vini já faz isso pra mim!
+
+        # ========================================
 
         tipo = tipoDEtreino()
 
@@ -258,8 +415,9 @@ while True:
                        "\nINTENSIDADE DO TREINO: " + intensidade_final)
 
         for i in range(len(treinos)):
-            if "NOME DO TREINO: " + treino_antigo.upper().strip() in treinos[i]:
+            if "NOME DO TREINO: " + treino_antigo.upper().strip() + "\n" in treinos[i]: # o \n impede que vc mude treinos com nomes parecidos (Ex: braços A, braços B)
                 treinos[i] = dados_novos
+                break
 
         novo_conteudo = "\n\n".join(treinos)
 
@@ -288,6 +446,35 @@ while True:
             else:
                 clear()
                 break
+        
+        # ========================================
+        # Essa parte do código procura o treino antigo no controle de desempenhos
+
+        if "EXERCICIOS:" in treinos[-1]: 
+
+            desempenhos = (treinos[-1]).split("\n---\n")
+
+            achou = False
+            for o in range(1, len(desempenhos)):
+                if treino_excluir.lower() + "\n" in desempenhos[o]:
+                    #esse_desempenho = desempenhos[i] #nesse caso, como eu só presciso deletar, não tem motivo pra dividir esse desempenho em linhas
+                    index = o
+                    achou = True
+                    break
+            
+            if achou:
+                
+                if len(desempenhos) <= 2: 
+                    treinos.pop(-1) # se desempenhos for ["EXERCICIOS:", desempenhos[i]], é melhor deletar tudo logo do que deixar só "EXERCICIOS:" no final do arquivo
+
+                else:
+                    desempenhos.pop(o) # remove esse desempenho
+                    
+                    treinos[-1] = "\n---\n".join(desempenhos) # atualiza o último índice de treinos
+
+                    # e, denovo, nn prescisa atualizar o arquivo aqui
+
+        # ========================================
 
         conjunto_fica = []
         
@@ -303,14 +490,28 @@ while True:
         treino.write(conjunto_fica)
         treino.close()
         print("Treino Excluído Com Sucesso!\n\n")
+        
 
-    elif opcao_escolhida == "6":
+   elif opcao_escolhida == "6":
+        treinoCD = input("Digite um treino para adicionar um desempenho: ")
 
+        treino = open("Sistema de Treinos.txt", "r")
+        conteudo = treino.read()
+        if "NOME DO TREINO: " + treinoCD.upper() in conteudo:
+            # só deixa adicionar um treino ao controle de desempenho se ele existir no crude,
+            # ⚠️ mas não impede o nome de ficar diferente aqui se o treino original for renomeado ou deletado
+            controledesempenho(treinoCD)
+        else:
+            print("Treino inexistente, adicione ele pelo CRUDE primeiro!")
+        
+
+
+   elif opcao_escolhida == "7":
+        import datetime
         nome_comp = input("Digite o nome da competição:")
         while True:
             data_comp = input("Digite a data da competição (DD/MM/AAAA): ")
             try:
-
                 datetime.datetime.strptime(data_comp.strip(), "%d/%m/%Y").date()
                 break
 
@@ -325,12 +526,12 @@ while True:
         print("Competição adicionada com sucesso!\n\n")
 
 
-    elif opcao_escolhida == "7":
+   elif opcao_escolhida == "8":
         print("Programa Finalizado!")
         break
 
 
-    else:
+   else:
         print("Opção inválida!")
         if not pergunta():
             break
